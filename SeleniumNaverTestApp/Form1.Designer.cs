13f196ea61e6ba45a7a29410beb3fe9c7bb71d73
neople <neople82@naver.com>
2022-01-25 16:19:16 +0900

    초기 커밋.

---
+++ SeleniumNaverTestApp/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace SeleniumNaverTestApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.metroStyleManager1 = new MetroFramework.Components.MetroStyleManager(this.components);
+            this.metroTile1 = new MetroFramework.Controls.MetroTile();
+            this.metroRadioButton1 = new MetroFramework.Controls.MetroRadioButton();
+            this.LogBox = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // metroStyleManager1
+            // 
+            this.metroStyleManager1.Owner = null;
+            this.metroStyleManager1.Style = MetroFramework.MetroColorStyle.Black;
+            this.metroStyleManager1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // metroTile1
+            // 
+            this.metroTile1.ActiveControl = null;
+            this.metroTile1.Location = new System.Drawing.Point(9, 54);
+            this.metroTile1.Name = "metroTile1";
+            this.metroTile1.Size = new System.Drawing.Size(116, 59);
+            this.metroTile1.TabIndex = 5;
+            this.metroTile1.Text = "Start";
+            this.metroTile1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.metroTile1.TileTextFontSize = MetroFramework.MetroTileTextSize.Tall;
+            this.metroTile1.TileTextFontWeight = MetroFramework.MetroTileTextWeight.Regular;
+            this.metroTile1.UseSelectable = true;
+            this.metroTile1.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // metroRadioButton1
+            // 
+            this.metroRadioButton1.AutoSize = true;
+            this.metroRadioButton1.Location = new System.Drawing.Point(131, 98);
+            this.metroRadioButton1.Name = "metroRadioButton1";
+            this.metroRadioButton1.Size = new System.Drawing.Size(52, 15);
+            this.metroRadioButton1.TabIndex = 6;
+            this.metroRadioButton1.Text = "Show";
+            this.metroRadioButton1.UseSelectable = true;
+            // 
+            // LogBox
+            // 
+            // 
+            // 
+            // 
+            this.LogBox.CustomButton.Image = null;
+            this.LogBox.CustomButton.Location = new System.Drawing.Point(-36, 2);
+            this.LogBox.CustomButton.Name = "";
+            this.LogBox.CustomButton.Size = new System.Drawing.Size(361, 361);
+            this.LogBox.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.LogBox.CustomButton.TabIndex = 1;
+            this.LogBox.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.LogBox.CustomButton.UseSelectable = true;
+            this.LogBox.CustomButton.Visible = false;
+            this.LogBox.Lines = new string[0];
+            this.LogBox.Location = new System.Drawing.Point(9, 128);
+            this.LogBox.MaxLength = 32767;
+            this.LogBox.Multiline = true;
+            this.LogBox.Name = "LogBox";
+            this.LogBox.PasswordChar = '\0';
+            this.LogBox.ReadOnly = true;
+            this.LogBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.LogBox.SelectedText = "";
+            this.LogBox.SelectionLength = 0;
+            this.LogBox.SelectionStart = 0;
+            this.LogBox.ShortcutsEnabled = true;
+            this.LogBox.Size = new System.Drawing.Size(328, 366);
+            this.LogBox.TabIndex = 7;
+            this.LogBox.UseSelectable = true;
+            this.LogBox.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.LogBox.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.metroLabel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.metroLabel1.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel1.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.metroLabel1.ForeColor = System.Drawing.Color.White;
+            this.metroLabel1.LabelMode = MetroFramework.Controls.MetroLabelMode.Selectable;
+            this.metroLabel1.Location = new System.Drawing.Point(9, 19);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(84, 25);
+            this.metroLabel1.Style = MetroFramework.MetroColorStyle.Brown;
+            this.metroLabel1.TabIndex = 8;
+            this.metroLabel1.Text = "대백마트";
+            this.metroLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 503);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.LogBox);
+            this.Controls.Add(this.metroRadioButton1);
+            this.Controls.Add(this.metroTile1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private MetroFramework.Components.MetroStyleManager metroStyleManager1;
+        private MetroFramework.Controls.MetroTile metroTile1;
+        private MetroFramework.Controls.MetroRadioButton metroRadioButton1;
+        private MetroFramework.Controls.MetroTextBox LogBox;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+    }
+}
+
---
+++ SeleniumNaverTestApp/Form1.cs
@@ -0,0 +1,140 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using System.Threading;
+using MetroFramework.Forms;
+using MetroFramework.Components;
+using MetroFramework;
+
+namespace SeleniumNaverTestApp
+{
+    public partial class Form1 : MetroForm
+    {
+
+        protected ChromeDriverService driverService = null;
+        protected ChromeOptions options = null;
+        protected ChromeDriver driver = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //!드라이버 서비스 추가
+            driverService = ChromeDriverService.CreateDefaultService();
+            //!true 숨기기, false 보이기
+            driverService.HideCommandPromptWindow = true;
+
+            options = new ChromeOptions();
+            options.AddArgument("disable-gpu");//!disable-gpu
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.FormClosed += Form_Closing;
+            StyleManager = metroStyleManager1;
+            metroStyleManager1.Theme = MetroFramework.MetroThemeStyle.Light;
+            metroStyleManager1.Style = MetroColorStyle.Magenta;
+
+        }
+
+        public void Form_Closing(object sender, FormClosedEventArgs e)
+        {
+            if (driver != null)
+            {
+                driver.Quit();
+                writeRichLog(($"[{DateTime.Now}] : Driver 제거 완료."));
+            }
+        }
+
+        void muteSount(bool type)
+        {
+            string a = "";
+            if (type)
+            {
+                a = "100% 볼륨";
+                writeRichLog(($"[{DateTime.Now}] : 음소거 해제 완료."));
+            }
+            else {
+                a = "100% 볼륨 음소거됨";
+                writeRichLog(($"[{DateTime.Now}] : 음소거 완료."));
+            }
+
+            var muteString = driver.FindElement(By.XPath(@"//*[@id='movie_player']/div/div/div/span/div")).GetAttribute("aria-valuetext");//!음소거버튼
+
+            if (muteString == a)
+            {
+                driver.FindElement(By.XPath(@"//*[@id='movie_player']/div/div[2]/div[1]/span/button")).Click();
+            }
+        }
+
+        private void writeRichLog(string log)
+        {
+            if (LogBox.Text.Length > 3_000) LogBox.Text = ""; // 3000자 표현후 텍스트 클리어. (성능 부분 고려) 
+            LogBox.AppendText(log + Environment.NewLine);
+            LogBox.SelectionStart = LogBox.Text.Length;
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {            
+            driver = new ChromeDriver(driverService, options);
+
+            writeRichLog(($"[{DateTime.Now}] : Driver 생성 완료."));
+            try
+            {
+                driver.Navigate().GoToUrl("https://www.youtube.com/results?search_query=%EB%A9%9C%EB%A1%A0+100");
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            }
+            catch (Exception ex)
+            {
+               writeRichLog(($"[{DateTime.Now}] : 웹페이지 오픈 실패,  사유 : .{ ex }"));
+               return;
+            }
+
+            writeRichLog(($"[{DateTime.Now}] : 웹페이지 오픈."));
+
+            try
+            {
+                driver.FindElement(By.XPath(@"//*[@id='contents']/ytd-video-renderer[1]")).Click();
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            }
+            catch
+            {
+                writeRichLog(($"[{DateTime.Now}] : 항목 선택 실패."));
+            }
+
+            writeRichLog(($"[{DateTime.Now}] : 항목 선택 완료."));
+            muteSount(true);
+
+
+            writeRichLog(($"[{DateTime.Now}] : 광고 건너뛰기 감지 시작."));
+            while (true)
+            {
+                bool skip = driver.FindElement(By.XPath(@"//div[text()='광고 건너뛰기']")).Displayed;//!광고 건너뛰기 찾기
+                if (skip)
+                {
+                    driver.FindElement(By.XPath(@"//div[text()='광고 건너뛰기']")).Click();//!광고 건너뛰기 찾을시 클릭
+                    break;
+                }
+            }
+
+            writeRichLog(($"[{DateTime.Now}] : 광고 건너뛰기 완료."));
+            IWebElement searchBtn = driver.FindElement(By.XPath("//*[@id='movie_player']/div[1]"));
+            Actions actionProvider = new Actions(driver);
+            actionProvider.ContextClick(searchBtn).Build().Perform();//!마우스 오른쪽 클릭
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.XPath(@"//div[text()='연속 재생']")).Click();//!광고 건너뛰기 찾을시 클릭
+
+            writeRichLog(($"[{DateTime.Now}] : 반복 재생 시작"));
+
+            muteSount(false);
+        }
+    }
+}
